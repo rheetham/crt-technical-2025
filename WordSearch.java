5aaae542d5949fba28a46769c8937c717b83b60c
Rheetham Menon <rheethammenon@gmail.com>
2025-03-25 16:08:07 +0530

    Edited word search

--- WordSearch.java
+++ WordSearch.java
@@ -1,3 +1,47 @@
+// public class WordSearch {
+//     public boolean exist(char[][] board, String word) {
+//         int m = board.length;
+//         int n = board[0].length;
+
+//         boolean[][] visited = new boolean[m][n];
+//         boolean result = false;
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (board[i][j] == word.charAt(0)) {
+//                     result = backtrack(board, word, visited, i, j, 0);
+//                     if (result)
+//                         return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+
+//     private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+//         if (index == word.length()) {
+//             return true;
+//         }
+
+//         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]
+//                 || board[i][j] != word.charAt(index)) {
+//             return false;
+//         }
+
+//         visited[i][j] = true;
+
+//         if (backtrack(board, word, visited, i + 1, j, index + 1) ||
+//                 backtrack(board, word, visited, i - 1, j, index + 1) ||
+//                 backtrack(board, word, visited, i, j + 1, index + 1) ||
+//                 backtrack(board, word, visited, i, j - 1, index + 1)) {
+//             return true;
+//         }
+
+//         visited[i][j] = false;
+//         return false;
+//     }
+// }
+
 public class WordSearch {
     public static void main(String args[]) {
         char board[][] = new char[3][4];
@@ -28,7 +72,8 @@ public class WordSearch {
         }
 
         for (int i = 1; i < m * n; i++) {
-            if (rows > 0 && board[rows - 1][cols] == word.charAt(wordPos) && !visited[rows - 1][cols]) {
+            if (rows > 0 && board[rows - 1][cols] == word.charAt(wordPos) &&
+                    !visited[rows - 1][cols]) {
                 wordPos++;
                 if (wordFound(wordPos, word)) {
                     return true;
@@ -37,7 +82,8 @@ public class WordSearch {
                 visited[rows][cols] = true;
                 continue;
             }
-            if (cols > 0 && board[rows][cols - 1] == word.charAt(wordPos) && !visited[rows][cols - 1]) {
+            if (cols > 0 && board[rows][cols - 1] == word.charAt(wordPos) &&
+                    !visited[rows][cols - 1]) {
                 wordPos++;
                 if (wordFound(wordPos, word)) {
                     return true;
@@ -46,7 +92,8 @@ public class WordSearch {
                 visited[rows][cols] = true;
                 continue;
             }
-            if (rows < m - 1 && board[rows + 1][cols] == word.charAt(wordPos) && !visited[rows + 1][cols]) {
+            if (rows < m - 1 && board[rows + 1][cols] == word.charAt(wordPos) &&
+                    !visited[rows + 1][cols]) {
                 wordPos++;
                 if (wordFound(wordPos, word)) {
                     return true;
@@ -55,7 +102,8 @@ public class WordSearch {
                 visited[rows][cols] = true;
                 continue;
             }
-            if (cols < n - 1 && board[rows][cols + 1] == word.charAt(wordPos) && !visited[rows][cols + 1]) {
+            if (cols < n - 1 && board[rows][cols + 1] == word.charAt(wordPos) &&
+                    !visited[rows][cols + 1]) {
                 wordPos++;
                 if (wordFound(wordPos, word)) {
                     return true;
