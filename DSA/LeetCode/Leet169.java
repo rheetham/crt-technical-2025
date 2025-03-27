a7d7e22dfe37e010699df14175e64e96846f82dd
Rheetham Menon <rheethammenon@gmail.com>
2025-03-27 14:42:33 +0530

    leetcode169

---
+++ DSA/LeetCode/Leet169.java
@@ -0,0 +1,24 @@
+class Leet169 {
+    static public int majorityElement(int[] nums) {
+        int freq = 1;
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (freq == 1) {
+                result = nums[i];
+            }
+            if (result == nums[i]) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 2, 3, 2, 2 };
+        System.out.println(majorityElement(arr));
+    }
+}
