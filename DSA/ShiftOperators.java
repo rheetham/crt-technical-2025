6c6cf99495c98a4702270f3dfef0dd0f10c10f0d
Rheetham Menon <rheethammenon@gmail.com>
2025-03-28 09:43:42 +0530

    first commit of the day

---
+++ DSA/ShiftOperators.java
@@ -0,0 +1,11 @@
+class ShiftOperators {
+
+    public static void main(String args[]) {
+        int num = 10;
+        int right = num >> 1;
+        int left = num << 1;
+        System.out.println(right);
+        System.out.println(left);
+    }
+
+}
