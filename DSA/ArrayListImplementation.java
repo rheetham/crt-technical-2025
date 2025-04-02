d63850fb831e1e6b7f21f7ab164a8a0d830e5bcd
Rheetham Menon <rheethammenon@gmail.com>
2025-04-02 16:37:43 +0530

    array list implementation

---
+++ DSA/ArrayListImplementation.java
@@ -0,0 +1,62 @@
+class MyArrayList {
+    private Object arr[];
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+
+    MyArrayList() {
+        arr = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public void add(Object obj) {
+        if (size == arr.length) {
+            resize();
+        }
+        arr[size++] = obj;
+    }
+
+    public void add(int index, Object obj) {
+        if (size == arr.length) {
+            resize();
+        }
+
+        Object newArr[] = new Object[size++];
+
+        for (int i = 0, j = 0; i < size; ++i) {
+            if (i == index && index >= 0 && index <= size) {
+                newArr[j++] = obj;
+                continue;
+            }
+            newArr[i] = arr[j++];
+        }
+        arr = newArr;
+    }
+
+    public void display() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public void resize() {
+        Object newArr[] = new Object[arr.length * 2];
+
+        for (int i = 0; i < arr.length; ++i) {
+            newArr[i] = arr[i];
+        }
+
+        arr = newArr;
+    }
+}
+
+class ArrayListImplementation {
+    public static void main(String[] args) {
+        MyArrayList list = new MyArrayList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(2, 0);
+        list.display();
+    }
+}
--- DSA/OptiMerge.java
+++ DSA/OptiMerge.java
@@ -1,6 +1,6 @@
 class OptiMerge {
     public static void main(String[] args) {
-        int arr1[] = { 1, 2, 7, 9, 10 };
+        int arr1[] = { 1, 2, 7, 9, 10, 11, 12, 13 };
         int arr2[] = { 3, 5, 7, 15 };
         int sorted[] = new int[arr1.length + arr2.length];
         int i = 0;
