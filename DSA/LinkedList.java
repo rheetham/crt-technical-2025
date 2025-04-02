2c6fa265f0bf504fbfb2d7056affda67c8cc66f6
Rheetham Menon <rheethammenon@gmail.com>
2025-04-02 09:22:41 +0530

    Linked List Example

--- DSA/LinkedList.java
+++ DSA/LinkedList.java
@@ -1,14 +1,21 @@
-class LinkedList {
-    public static void main(String[] args) {
-        Node listNode = new Node(5);
-    }
-}
-
 class Node {
     int value;
     Node next;
 
     Node(int value) {
         this.value = value;
+        this.next = null;
+    }
+
+    void display() {
+        System.out.println(this.value);
+        System.out.println(this.next);
+    }
+}
+
+class LinkedList {
+    public static void main(String[] args) {
+        Node listNode = new Node(5);
+        listNode.display();
     }
 }
