96ad63856b182d1ff7a55d46b60011fddb3b7af1
Rheetham Menon <rheethammenon@gmail.com>
2025-04-03 10:54:40 +0530

    modified test

--- DSA/Test.java
+++ DSA/Test.java
@@ -1,23 +1,10 @@
 class Test {
-    static void insertionSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; ++i) {
-            for (int j = i + 1; j > 0; --j) {
-                if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
+    public static void main(String args[]) {
+        int i = 10;
 
-        for (int i : arr) {
+        if (true) {
+            int i = 20;
             System.out.println(i);
         }
-    }
-
-    public static void main(String args[]) {
-        int arr[] = { 3, 3, 4, 1, 3, 12 };
-        insertionSort(arr);
     }
 }
